e57f519a09344552540f91dc56802079a84c740d
Lukas Nickel <lukas.nickel@tu-dortmund.de>
2017-11-08 13:45:02 +0100

    c done, not sure about anything but the core should be ok, just the RNG's might be messing around

--- Blatt3/energy.py
+++ Blatt3/energy.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def phi(phi0, E, gamma):
     return (phi0*(E)**(-gamma))
 
@@ -8,6 +9,25 @@ def phi_mc(gamma, phi0, x):
     inv = (1-x)**(1-gamma)
     return inv
 
+
 def acceptance_mc(E):
     p = (1-np.exp(-E/2))**3
     return p
+
+
+def hits_mc(E):
+    inside = False
+    while inside is False:
+        u1 = np.random.uniform(0, 1, 1)
+        u2 = np.random.uniform(0, 1, 1)
+        v1 = 2*u1-1
+        v2 = 2*u2-1
+        s = v1**2+v2**2
+        if s < 1:
+            inside = True
+    x1 = v1*np.sqrt(-2/s*np.log(s))
+    x2 = v2*np.sqrt(-2/s*np.log(s))  # Das sollten jetzt 2 normalverteilte ZZ sein?
+    mu = 10*E
+    sigma = 2*E
+    x_E = sigma * x1 + sigma * x2 + mu
+    return x_E
--- Blatt3/main.py
+++ Blatt3/main.py
@@ -3,16 +3,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from energy import phi_mc
 from energy import acceptance_mc
+from energy import hits_mc
 
-uni = np.random.uniform(0, 1, 10**5)
+N = 100
+uni = np.random.uniform(0, 1, N)
 E = phi_mc(2.7, 5, uni)  # Passt noch nicht
-
 data = pd.DataFrame(E, columns = ['Energy'])
-print(data.mean())
 
-acceptance_uni = np.random.uniform(0, 1, 10**5)
+acceptance_uni = np.random.uniform(0, 1, N)
 diff = acceptance_uni - acceptance_mc(E)
 acceptance_mask = [(x>0) for x in diff]
 
 data['AcceptanceMask'] = acceptance_mask
+
+hits = hits_mc(E)
+
+data['NumberOfHits'] = hits
+
 print(data)
