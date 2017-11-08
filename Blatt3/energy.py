f3bfcdc66e961d42a43540951cb46cc150e42d04
Lukas Nickel <lukas.nickel@tu-dortmund.de>
2017-11-08 13:00:22 +0100

    energy seems reasonable now, but doesnt fit inverse function? maybe mistake on my part

--- Blatt3/energy.py
+++ Blatt3/energy.py
@@ -3,7 +3,5 @@ def phi(phi0, E, gamma):
 
 
 def phi_mc(gamma, phi0, x):
-    inv = (1-x*gamma/phi0)**(gamma-1)
-    A = phi0/(gamma+1)  # Normierung
-    inv_norm = inv/A
-    return inv_norm
+    inv = (1-x)**(1-gamma)
+    return inv
