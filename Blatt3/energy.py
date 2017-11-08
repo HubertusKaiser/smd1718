5d2de278bb61a52b6f01e1c38f1d50900190890d
Lukas Nickel <lukas.nickel@tu-dortmund.de>
2017-11-08 13:13:12 +0100

    a and b kinda working, might be wrong

--- Blatt3/energy.py
+++ Blatt3/energy.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def phi(phi0, E, gamma):
     return (phi0*(E)**(-gamma))
 
@@ -5,3 +7,7 @@ def phi(phi0, E, gamma):
 def phi_mc(gamma, phi0, x):
     inv = (1-x)**(1-gamma)
     return inv
+
+def acceptance_mc(E):
+    p = (1-np.exp(-E/2))**3
+    return p
--- Blatt3/main.py
+++ Blatt3/main.py
@@ -2,14 +2,17 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from energy import phi_mc
+from energy import acceptance_mc
 
 uni = np.random.uniform(0, 1, 10**5)
 E = phi_mc(2.7, 5, uni)  # Passt noch nicht
 
 data = pd.DataFrame(E, columns = ['Energy'])
-print(data)
 print(data.mean())
 
-plt.hist(E)
-plt.show()
-plt.clf()
+acceptance_uni = np.random.uniform(0, 1, 10**5)
+diff = acceptance_uni - acceptance_mc(E)
+acceptance_mask = [(x>0) for x in diff]
+
+data['AcceptanceMask'] = acceptance_mask
+print(data)
