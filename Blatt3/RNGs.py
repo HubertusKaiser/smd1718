385b37b8a60782f371005f2a6422a29eb22fc659
Lukas Nickel <lukas.nickel@tu-dortmund.de>
2017-11-18 17:52:55 +0100

    blub

--- Blatt3/RNGs.py
+++ Blatt3/RNGs.py
@@ -42,3 +42,25 @@ def normal_polar(mux, muy, sigmax, sigmay, rho):
     x_ms = np.sqrt(1-rho**2)*sigmax * x1 + rho*sigmay * x2 + mux
     y_ms = sigmay*x2 + muy
     return [x_ms, y_ms]
+
+def normal_polar_std(N):
+    x = np.empty(N)
+    y = np.empty(N)
+    for (u, v) in zip(x, y):
+        u, v = get_random()
+    return [x, y]
+    
+def get_random():
+    s = 2
+    while s > 1 or s < 0:
+        u1 = np.random.uniform(0, 1, 1)
+        u2 = np.random.uniform(0, 1, 1)
+        v1 = 2*u1-1
+        v2 = 2*u2-1
+        s = v1**2+v2**2
+        if np.isnan(np.sqrt(-2/s*np.log(s))):
+            s = 2
+    x1 = v1*np.sqrt(-2/s*np.log(s))
+    x2 = v2*np.sqrt(-2/s*np.log(s))
+    return [x1, x2]
+    
--- Blatt3/main.py
+++ Blatt3/main.py
@@ -6,13 +6,16 @@ from RNGs import acceptance_mc
 from RNGs import hits_mc
 from RNGs import position_mc
 from RNGs import normal_polar
-
+from RNGs import normal_polar_std
 
 def ex_a():
     gamma_sig = 2.7
     uniform = np.random.uniform(0, 1, N_sig)
     E = energy_mc(gamma_sig, uniform)  # uses transformation method
     data['Energy'] = E
+    plt.hist(E, bins = 20)
+    plt.savefig('build/E.pdf')
+    plt.clf()
 
 
 def ex_b():
@@ -20,19 +23,30 @@ def ex_b():
     diff = acceptance_uni - acceptance_mc(data.Energy.values)  # rej method(?)
     acceptance_mask = [(x > 0) for x in diff]
     data['AcceptanceMask'] = acceptance_mask
+    # wie plotten?
 
 
 def ex_c():
     hits, hits_ = hits_mc(data.Energy.values)  # uses polar method
-    data['NumberOfHits'] = hits
+    data['NumberOfHits'] = np.round(hits)
 
 
 def ex_d():
-    mux_sig = 7  # mu of position normal dist
+    mux_sig = 7 
     muy_sig = 3
     sigma_sig = 1/np.log10(data.NumberOfHits.values+1)
-    x, y = position_mc(mux_sig, muy_sig, sigma_sig, sigma_sig, 0)
-    # y, y_ = position_mc(mux_sig, muy_sig, sigma_sig, 0)
+    x, y = normal_polar_std(N_sig) #MCMC?
+    x = x*sigma_sig + mux_sig
+    y = y*sigma_sig + muy_sig
+    for (a,b) in zip(x,y):   # hart ineffizient
+        inside = False
+        while not inside:
+            if a.all() < 10 and b.all() < 10 and a.all() > 0 and b.all() > 0:
+                inside = True
+            if a.all() > 10 or a.all() < 0:
+                a = normal_polar_std(1) * sigma_sig + mux_sig
+            if b.all() > 10 or b.all() < 0:
+                b = normal_polar_std(1) * sigma_sig + muy_sig
     data['x'] = x
     data['y'] = y
 
@@ -45,34 +59,38 @@ def ex_d():
 def ex_e():
     mu_bkgx = 2
     mu_bkgy = mu_bkgx
-    N_bkg = 10**7
+    N_bkg = 10**3
     sigma_bkgx = np.ones(N_bkg)
     sigma_bkgy = sigma_bkgx
     rho_bkghits = 0
-    hits_bkg, hits_ = 10**np.array(normal_polar(mu_bkgx, mu_bkgy, sigma_bkgx, sigma_bkgy, rho_bkghits) ) # log10
-    rho_bkgpos = 0.5
-    x_bkg, y_bkg = position_mc(5, 5, np.ones(N_bkg)*3, np.ones(N_bkg)*3, rho_bkgpos)
-    # y_bkg, y_ = position_mc(5, 5, np.ones(N_bkg)*3)
-    bkg = pd.DataFrame()
-    bkg['NumberOfHits'] = hits_bkg
+    t,t2 = normal_polar_std(N_bkg)
+    hits_bkg_log = t*2+1 # log10
+    rho = 0.5
+    x_bkg, y_bkg = normal_polar_std(N_bkg)
+    x_bkg = x_bkg * sigma_bkgx * np.sqrt(1-rho**2) + rho * sigma_bkgy * y_bkg + mu_bkgx
+    y_bkg = y_bkg * sigma_bkgy + mu_bkgy
+    bkg['NumberOfHits_log'] = hits_bkg_log
     bkg['x'] = x_bkg
     bkg['y'] = y_bkg
-    plt.hist2d(x_bkg, y_bkg, bins=[30, 30])
-    plt.colorbar()
-    plt.savefig('build/position_heatmap_bkg.pdf')
-    plt.clf()
+    #plt.hist2d(x_bkg, y_bkg, bins=[30, 30])
+    #plt.colorbar()
+    #plt.savefig('build/position_heatmap_bkg.pdf')
+    #plt.clf()
 
-    plt.hist(np.log10(hits_bkg))
-    plt.savefig('build/N_bkg.pdf')
-    plt.clf()
+    #plt.hist(t, bins = 20)
+    #plt.savefig('build/N_bkg.pdf')
+    #plt.clf()
 
 
 if __name__ == "__main__":
+    np.random.seed(12)
     N_sig = 10000
     data = pd.DataFrame()
+    bkg = pd.DataFrame()
     ex_a()
     ex_b()
     ex_c()
     ex_d()
     ex_e()
-    print(data)
+    data.to_hdf('build/NeutrinoMC.hd5', 'data')
+    bkg.to_hdf('build/NeutrinoMC.hd5', 'bkg')
