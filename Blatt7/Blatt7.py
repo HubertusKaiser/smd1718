f162b29af6e9af528cd16f36b3cfc793decca3fa
Lukas Nickel <lukas.nickel@tu-dortmund.de>
2017-12-12 15:34:09 +0100

    23 mehr oder weniger fertig

--- Blatt7/Blatt7.py
+++ Blatt7/Blatt7.py
@@ -2,7 +2,10 @@ import numpy as np
 import uncertainties as unc
 from uncertainties import unumpy as unp
 import matplotlib.pyplot as plt 
-from mc_class.py import MCMC
+from mc_class import MCMC
+from scipy.stats import norm
+import matplotlib.mlab as mlab
+
 
 
 def aufg23():
@@ -16,10 +19,50 @@ def aufg23():
     y_err = unp.std_devs(y(x_test))
     plt.plot(x_test, y_err, 'kx')
     plt.savefig('23_a.pdf')
-    
+    plt.clf()
    # scs.norm(loc, scale).pdf
-    a = MCMC(step_size = 2, loc = -3, scale = 2, y_pdf)  
-    y = a.sample(x0 = 15, n = size)
+    #a = MCMC(step_size = 2, loc = -3, scale = 2, y)  
+    #y = a.sample(x0 = 15, n = size)
+    
+    err = MCMC(step_size = 0.1, loc=0, scale=0.2)
+    np.random.seed(123)
+    err_1_vz = err.sample(x0 = 0, n=10000)
+    err_1 = np.abs(err_1_vz)
+    np.random.seed(231)
+    err_2_vz = err.sample(x0 = 0, n=10000)
+    err_2 = np.abs(err_2_vz)
+    
+    x = -1
+    rho = 0.8
+    y_err_mc = np.sqrt(err_1**2 + err_2**2*x**2+ 2*x*rho*err_1*err_2)  
+    #print(y_err_mc)
+    #print(y_err_mc)
+    
+    #plt.hist(y_err_mc)
+    #plt.show()
+
+    #print(mu, sigma)
+
+    plt.scatter(err_1_vz, err_2_vz, s=1)
+    plt.savefig('23_scatter.pdf')
+    plt.clf()
+    
+    x_lin = np.linspace(-3,3,7)
+
+
+    for x in x_lin:   
+        #y_err_mc = np.sqrt(err_1_vz**2 + err_2_vz**2*x**2+ 2*x*rho*err_1_vz*err_2_vz)  
+        y_err_mc = np.sqrt(err_1**2 + err_2**2*x**2+ 2*x*rho*err_1*err_2)  
+        y_err_mc = np.concatenate((y_err_mc, -y_err_mc))   
+        n, bins, patches = plt.hist(y_err_mc, 60, normed=1, facecolor='green', alpha=0.75)
+        (mu, sigma) = norm.fit(y_err_mc)
+        y = mlab.normpdf(bins, mu, sigma)
+        lab = 'mu='+str(round(mu,3))+', sigma='+str(round(sigma,3))
+        l = plt.plot(bins, y, 'r--', linewidth=2, label=lab)
+        plt.legend()
+        plt.title('Fehler von y für x=' + str(x))
+        plt.savefig('hist_normed_' + str(x) + '.pdf')
+        plt.clf()
     
 if __name__ == "__main__":
     aufg23()
